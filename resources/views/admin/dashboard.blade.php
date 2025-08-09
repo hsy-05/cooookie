bb78fda5fde13b70f7e1bd05f96d2d923425fa19
hsy <xaoy.358@gmail.com>
2025-08-09 16:35:19 +0800

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('title', '管理後台')
+
+@section('content_header')
+    <h1>後台首頁</h1>
+@endsection
+
+@section('content')
+    <p>歡迎使用 AdminLTE 後台模板！</p>
+@endsection
