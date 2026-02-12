8d35bc1f39e8e1a75e0cf18a5f4185f9732f7c25
hsy <xaoy.358@gmail.com>
2026-02-12 15:01:49 +0800

    feat: 新增自定義全域樣式與側邊欄快捷按鈕組

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -9,6 +9,22 @@
 
     {{-- Sidebar menu --}}
     <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex justify-content-center">
+    <div class="btn-group btn-group-sm w-100 px-2" role="group">
+        <a href="{{ route('admin.dashboard') }}" class="btn btn-info" title="後台首頁">
+            <i class="fas fa-tachometer-alt"></i>
+        </a>
+        <a href="{{ url('/') }}" target="_blank" class="btn btn-success" title="前台首頁">
+            <i class="fas fa-home"></i>
+        </a>
+        <button type="button" id="btn-quick-clear" class="btn btn-warning" title="清除快取">
+            <i class="fas fa-broom"></i>
+        </button>
+        <a href="/" class="btn btn-secondary" title="系統設定">
+            <i class="fas fa-cogs"></i>
+        </a>
+    </div>
+</div>
         <nav class="pt-2">
             <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
                 data-widget="treeview" role="menu"
